66c35a507788a7cdf48cad6cd95213d5864dad8a
Nikita <322liquid@gmail.com>
2026-05-27 18:49:15 +0500

    bcrypt passlib problem solved

--- core/security.py
+++ core/security.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timedelta, timezone
+import hashlib
 from typing import Any, Union, Optional
+import bcrypt
 from jose import jwt
-from passlib.context import CryptContext
+# from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 from .config import settings
 
@@ -9,13 +11,15 @@ from .config import settings
 # tokenUrl — это путь, по которому Swagger будет отправлять логин/пароль для получения токена
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    pwd_encoded = hashlib.sha256(password.encode('utf-8')).hexdigest().encode('utf-8')
+    return bcrypt.hashpw(pwd_encoded, bcrypt.gensalt()).decode('utf-8')
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    pwd_encoded = hashlib.sha256(plain_password.encode('utf-8')).hexdigest().encode('utf-8')
+    return bcrypt.checkpw(pwd_encoded, hashed_password.encode('utf-8'))
 
 def create_access_token(subject: Union[str, Any], expires_delta: Optional[timedelta] = None) -> str:
     if expires_delta:
