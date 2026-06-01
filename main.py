088a854a02f429959bee6eea818ef19ca61a88d7
Nikita <322liquid@gmail.com>
2026-06-01 16:42:33 +0500

    cors middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from db.database import engine
 from models.base import Base
 from api import auth, users, courses, notes
@@ -7,6 +8,21 @@ from api import auth, users, courses, notes
 
 
 app = FastAPI(title="LMS API")
+
+
+origins = [
+    "http://localhost:5173",    
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            
+    allow_credentials=True,           
+    allow_methods=["*"],              
+    allow_headers=["*"],              
+)
+
 
 app.include_router(auth.router)
 app.include_router(users.router)
